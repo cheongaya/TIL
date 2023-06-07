aa33da2755a5986e9ca5db41fd722d542b735518
cheongaya <chengalongo@gmail.com>
2023-06-08 00:20:03 +0900

    Test : Code App ArrayList Code Add

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -1,9 +1,28 @@
 // Created on iPad.
+import java.util.ArrayList;
 
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
         System.out.println("Code App Test 22");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("kca");
+        names.add("jbs");
+
+        System.out.println(names);
+
+        ArrayList<String> colors = new ArrayList<>();
+        // add() method
+        colors.add("Black");
+        colors.add("White");
+        colors.add(0, "Green");
+        colors.add("Red");
+
+        // set() method
+        colors.set(0, "Blue");
+
+        System.out.println(colors);
     }
 }
