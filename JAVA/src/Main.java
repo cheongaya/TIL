55948fff682c36d63303e731b9ddd6b47cd43f9c
Cheonga <chengalongo@gmail.com>
2023-06-08 00:02:02 +0900

    Test : IPad Code App Test

---
+++ JAVA/src/Main.java
@@ -0,0 +1,9 @@
+// Created on iPad.
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println("Code App Test");
+    }
+}
